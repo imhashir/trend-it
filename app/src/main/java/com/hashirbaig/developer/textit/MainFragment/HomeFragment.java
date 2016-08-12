d9a02bd7440805cd7ae9d143da8d9d509f45b5e4
Hashir Baig <hashirbaig@gmail.com>
2016-08-13 01:33:38 +0500

    Added Permission Request

--- app/src/main/java/com/hashirbaig/developer/textit/MainFragment/HomeFragment.java
+++ app/src/main/java/com/hashirbaig/developer/textit/MainFragment/HomeFragment.java
@@ -1,9 +1,14 @@
 package com.hashirbaig.developer.textit.MainFragment;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +22,26 @@ public class HomeFragment extends Fragment{
     private ImageView mBrowseButton;
 
     private static final String TAG_IMAGE_CHOOSER = "image_chooser_dialog";
+    private static final int REQUEST_STORAGE_PERMISSION = 2001;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_STORAGE_PERMISSION) {
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                getActivity().finishAffinity();
+            }
+        }
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        }
     }
 
     public static HomeFragment newInstance() {
