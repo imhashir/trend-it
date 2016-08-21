aaca1f85dd39380c9bd2993afb7e400ad8e3a9a6
Hashir Baig <hashirbaig@gmail.com>
2016-08-21 14:05:09 +0500

    Implemented Drawer View and On Click Album

--- app/src/main/java/com/hashirbaig/developer/textit/Model/FiltersManager.java
+++ app/src/main/java/com/hashirbaig/developer/textit/Model/FiltersManager.java
@@ -2,21 +2,24 @@ package com.hashirbaig.developer.textit.Model;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.net.Uri;
 import android.util.Log;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FiltersManager {
 
-    private List<FilterImage> mList;
+    private List<FilterAlbum> mList;
     private static FiltersManager sFiltersManager;
     private Context mContext;
+    private FilterAlbum mCurrentAlbum;
 
     private final String TAG = "BackgroundManager";
 
-    private static final String IMAGE_FOLDER = "background_images";
+    private static final String IMAGE_FOLDER = "filter_images";
 
     private FiltersManager(Context context) {
         mList = new ArrayList<>();
@@ -26,19 +29,33 @@ public class FiltersManager {
 
     private void loadImages(){
 
-        String[] imagesNames = null;
+        String[] foldersNames = null;
         AssetManager manager = mContext.getAssets();
         try {
-            imagesNames = manager.list(IMAGE_FOLDER);
+            foldersNames = manager.list(IMAGE_FOLDER);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        for(String name : imagesNames) {
+        List<FilterImage> filterImages;
+        for(String name : foldersNames) {
             String path =  IMAGE_FOLDER + "/" + name;
-            FilterImage image = new FilterImage(path);
-            image.setName(name);
-            mList.add(image);
+            FilterAlbum album = new FilterAlbum(path);
+            album.setName(name);
+            String[] filterImagesNames = null;
+            try {
+                filterImagesNames = manager.list(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            filterImages = new ArrayList<>();
+            for(String filterName : filterImagesNames) {
+                FilterImage image = new FilterImage(path + "/" + filterName);
+                image.setName(filterName);
+                filterImages.add(image);
+            }
+            album.setFilterImages(filterImages);
+            mList.add(album);
             Log.i(TAG, path);
         }
     }
@@ -50,11 +67,19 @@ public class FiltersManager {
         return sFiltersManager;
     }
 
-    public void add(FilterImage image) {
-        mList.add(image);
+    public List<FilterImage> getFramesList() {
+        return mCurrentAlbum.getFilterImages();
     }
 
-    public List<FilterImage> getList() {
+    public List<FilterAlbum> getList() {
         return mList;
+    }
+
+    public FilterAlbum getCurrentAlbum() {
+        return mCurrentAlbum;
+    }
+
+    public void setCurrentAlbum(FilterAlbum currentAlbum) {
+        mCurrentAlbum = currentAlbum;
     }
 }
