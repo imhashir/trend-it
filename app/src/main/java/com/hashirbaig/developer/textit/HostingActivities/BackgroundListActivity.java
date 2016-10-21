f2b1b9403b9607cdc2495f401f1d4b71454f8fdd
imhashir <hashirbaig@gmail.com>
2016-10-21 18:54:00 +0500

    Compiled for SDK version 25

--- app/src/main/java/com/hashirbaig/developer/textit/HostingActivities/BackgroundListActivity.java
+++ app/src/main/java/com/hashirbaig/developer/textit/HostingActivities/BackgroundListActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -100,6 +99,8 @@ public class BackgroundListActivity extends AppCompatActivity {
             FiltersManager.get(mContext).setCurrentAlbum(mAlbum);
             mListFragment.updateUI();
             mDrawerLayout.closeDrawers();
+            view.setBackgroundColor(getResources().getColor(R.color.selected_list_view_background));
+            mTextView.setTextColor(getResources().getColor(R.color.selected_list_text_color));
         }
     }
 
